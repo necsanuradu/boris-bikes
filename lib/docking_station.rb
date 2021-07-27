a0c71b8b6b8c9bc8ecb6391f8137a90d2632b54e
Radu Necsanu <necsanu_radu@yahoo.com>
2021-07-27 16:28:04 +0100

    error release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,13 @@
 class DockingStation
+ 
+  def release_bike
+    return true
+  end
 
 end
+=begin
+def release_bike
+  return true
+end
+=end
+docking_station = DockingStation.new
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require "./lib/docking_station.rb"
 
 describe "DockingStation" do
-
+ it "it should release a bike" do 
+  expect(DockingStation.release_bike).to eq(true)
+ end
 end
