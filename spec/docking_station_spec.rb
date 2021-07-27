6a401bdf5d813c63a98330c84b2f7394d46c53d4
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-07-27 17:40:02 +0100

    updated rspec tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,18 @@
 require "./lib/docking_station.rb"
 
 describe DockingStation do
- it "responds to release_bike" do 
-  expect(subject).to respond_to :release_bike
- end
+  it "responds to release_bike" do 
+    expect(subject).to respond_to :release_bike
+  end
 
- it "it gets a bike" do 
-    expect(subject.release_bike).instance_of? Object
- end
+  it "it gets a bike" do 
+    expect(subject.release_bike.instance_of? Bike).to eq(true)
+  end
 
+  it "expects the bike to be working" do 
+    expect(subject.release_bike.working?).to eq(true)
+  end 
 end
 
-=begin
- it "it gets a bike" do 
-   expect(subject.release_bike.working?).to eq(true)
-   # instance_of? Object
- end 
- =end
+
+
