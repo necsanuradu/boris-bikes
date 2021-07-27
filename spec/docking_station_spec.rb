d02afd8c78118d0106c429093ff3788865571444
Radu Necsanu <necsanu_radu@yahoo.com>
2021-07-27 17:29:24 +0100

    bike and working? tests added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,16 @@ describe DockingStation do
  it "responds to release_bike" do 
   expect(subject).to respond_to :release_bike
  end
+
+ it "it gets a bike" do 
+    expect(subject.release_bike).instance_of? Object
+ end
+
 end
+
+=begin
+ it "it gets a bike" do 
+   expect(subject.release_bike.working?).to eq(true)
+   # instance_of? Object
+ end 
+ =end
