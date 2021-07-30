af7c98679c9a0226ef427bde0f10f214d752511b
Radu Necsanu <necsanu_radu@yahoo.com>
2021-07-30 11:31:25 +0100

    working chapter18

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,24 +6,37 @@ class Object
   end
 end
 class DockingStation
-  attr_reader :docked_bikes
-  def initialize
-    @docked_bikes = Array.new
+  attr_reader :working_bikes, :capacity
+  DEFAULT_CAPACITY = 20
+  def initialize(default_capacity = DEFAULT_CAPACITY)
+    @capacity = default_capacity
+    @working_bikes = Array.new
+    @broken_bikes = Array.new
   end
 
-  def release_bike
-    if(!@docked_bikes.empty?)
-      Bike.new
-    else 
-      raise "No bikes available"
-    end
+  def release_bike(how_many = 1)
+    raise "Not enough working bikes available" if not_enough?(how_many)
+    @bikes_to_be_released = @working_bikes.slice!(0,how_many)
+    @bikes_to_be_released.last
   end
 
-  def docking_bike(bike)
-    @bike = bike
-    raise "Dock is full" unless @docked_bikes.count < 1
-    @docked_bikes.push(@bike)
-    return @bike
+  def docking_bike(bikes)
+    bikes.respond_to?("each") ? @bikes = bikes : @bikes = [bikes]
+    raise "Not enough spaces available" if too_many?(@bikes)
+    filter_docking_bikes
+    @bikes.last
+  end
+
+  def filter_docking_bikes
+    @bikes.each { |bike| bike.working? ? (@working_bikes << bike) : (@broken_bikes << bike) }
+  end
+  private
+  def not_enough?(how_many)
+    how_many > @working_bikes.count
+  end
+
+  def too_many?(bikes)
+    (@working_bikes.count + @broken_bikes.count + bikes.count) > @capacity
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require "./lib/docking_station.rb"
 # subject(:diary_printer) { described_class.new(diary, output) }
 describe DockingStation do
+  subject(:dock_with_set_capacity){ described_class.new(30) }
+  subject(:dock_with_default_capacity){ described_class.new }
   it "responds to release_bike" do 
     expect(subject).to respond_to :release_bike
   end
@@ -18,17 +20,26 @@ describe DockingStation do
   it "responds to docking_bike" do
     # bike = subject.docking_bike(Bike.new)
     # expect(subject.docked_bikes.include? (bike)).to eq (true)
-    expect(subject.docking_bike(Bike.new).in?(subject.docked_bikes)).to eq(true)
+    expect(subject.docking_bike(Bike.new).in?(subject.working_bikes)).to eq(true)
   end
 
   it "should not release bike if none are available" do 
-    expect{ subject.release_bike }.to raise_error "No bikes available"
+    expect{ subject.release_bike }.to raise_error "Not enough working bikes available"
   end 
 
   it "it raises an error if dock is full" do
-    subject.docking_bike(Bike.new)
-    expect{ subject.docking_bike(Bike.new) }.to raise_error "Dock is full"
+    dock_with_default_capacity.capacity.times{ subject.docking_bike(Bike.new) }
+    expect{ subject.docking_bike(Bike.new) }.to raise_error "Not enough spaces available"
   end
+
+  it "allows a capacity variable to be set with the initialize method" do
+    expect(dock_with_set_capacity.capacity).to eq(30)
+  end
+
+  it "check the default capacity to be equal to 20" do 
+    expect(dock_with_default_capacity.capacity).to eq(20)
+  end
+
 end
 
 
